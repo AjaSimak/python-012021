6afb21d4efc7a02126ecc25ee21ba21b283b947d
Your Name <you@example.com>
2021-02-22 17:43:27 +0100

    Hodnocení úkolů

---
+++ 1_lekce_ukoly/hodnoceni.py
@@ -0,0 +1,28 @@
+
+# 1. úkol : 0 bod
+# řádek 10 je super, ale na řádku 11 to padá
+#
+# Doporučuji se podívat co dělají tyhle dva řádky (vrací hodnotu pro daný klíč)
+# number = input("Zadej číslo balíku: ")
+# print(baliky[number])
+
+
+# 2. úkol : 0 bod
+# není soubor
+
+# 3. úkol : 1 bod
+# super
+# podařilo by se ti spočítat, kolik je volných místností?
+
+# 4. úkol : 0 bod
+# Musíš procházet jednotlivé znaky z řetězce zadaného uživatelem. ( 2. Projdi řetězec zadaný uživatelem. Najdi každý znak ve slovníku a vypiš ho na obrazovku v Morseově abecedě.)
+# zkus si napřed vytisknou pod sebe jednotlivé znaky, které zadal uživatel
+
+# 5. úkol : 0 bod
+# proč tam máš tento řádek?
+# 18 prodeje2019["Past"] = 0
+
+
+
+# Držím palce, ještě se na to podívej
+
