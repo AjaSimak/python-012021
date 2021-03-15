c60e812325cf5c4542b937838ce9c06a90978cbe
andre <andrea.stolfova@gmail.com>
2021-03-15 22:33:17 +0100

    Jestli jsem to pochopila správně, takto si to myslel?

--- 3_lekce_ukoly/priklad14.py
+++ 3_lekce_ukoly/priklad14.py
@@ -1,11 +1,11 @@
 class Employee:
   def get_info(self):
     return f"{self.name} pracuje na pozici {self.position}."
-  def __init__(self, name, position):
+  def __init__(self, name, position, salary, children):
     self.name = name
     self.position = position
-    self.salary = 20000
-    self.children = 6
+    self.salary = salary
+    self.children = children
 
   def get_net_salary(self):
     #self.cista_mzda = cista_mzda
@@ -15,6 +15,11 @@ class Employee:
     cista_mzda = self.salary - dan
     return f"Čistá mzda je {cista_mzda}."
 
-andrea = Employee("Andrea", "analytik")
+andrea = Employee("Andrea", "analytik", 20000, 6)
+petra = Employee("Petra", "datařka", 45000, 10)
+
+print(andrea.get_info())
 print(andrea.get_net_salary())
+print(petra.get_info())
+print(petra.get_net_salary())
 
