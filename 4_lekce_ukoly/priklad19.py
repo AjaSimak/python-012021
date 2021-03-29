139eeddb86778c90b7eee7f4ebd17f49530c1aed
andre <andrea.stolfova@gmail.com>
2021-03-29 20:56:08 +0200

    Ahoj Aleši, posílám opravu. Andrea

--- 4_lekce_ukoly/priklad19.py
+++ 4_lekce_ukoly/priklad19.py
@@ -1,14 +1,19 @@
 from forex_python.converter import CurrencyRates
 prevodnik = CurrencyRates()
+pozadovano_v_cilove_mene = int(input("Zadej množství cílové měny: "))
+pozadovana_mena = input("Zadej cizí měnu (EUR, USD, GBP, DKK): ")
 
-class CurrencyRates:
-  def convert(self, cilova_mena, zdrojova_mena, pozadovano_v_cilove_mene):
-    self.cilova_mena = cilova_mena
-    self.zdrojova_mena = zdrojova_mena
-    self.pozadovano_v_cilove_mene = pozadovano_v_cilove_mene
-    return cena_v_korunach
+cena_v_korunach_EUR = prevodnik.convert('EUR', 'CZK', pozadovano_v_cilove_mene)
+cena_v_korunach_USD = prevodnik.convert('USD', 'CZK', pozadovano_v_cilove_mene)
+cena_v_korunach_GBP = prevodnik.convert('GBP', 'CZK', pozadovano_v_cilove_mene)
+cena_v_korunach_DKK = prevodnik.convert('DKK', 'CZK', pozadovano_v_cilove_mene)
 
-pozadovano_v_cilova_mene = int(input("Kolik požadujete v cílové měně? "))
-cena_v_korunach = prevodnik.convert('USD', 'CZK', pozadovano_v_cilove_mene)
-print(cena_v_korunach.convert())
+if pozadovana_mena == "EUR":
+  print(cena_v_korunach_EUR)
+elif pozadovana_mena == "USD":
+  print(cena_v_korunach_USD)
+elif pozadovana_mena == "GBP":
+  print(cena_v_korunach_GBP)
+else:
+  print(cena_v_korunach_DKK)
 
