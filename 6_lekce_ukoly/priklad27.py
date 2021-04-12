caa9a19a106f2d6ba5ba417485fd1777a3076811
andre <andrea.stolfova@gmail.com>
2021-04-12 13:02:19 +0200

    Ahoj Aleši, posílám bonusový úkol.A.

--- 6_lekce_ukoly/priklad27.py
+++ 6_lekce_ukoly/priklad27.py
@@ -8,22 +8,15 @@ hodiny = vykazy.groupby("project")["hours"].sum()
 zamestnanci = pandas.read_csv("zamestnanci.csv")
 
 
-print(vykazy.loc[1])
-print(hodiny)
+#print(vykazy.loc[1])
+#print(hodiny)
 
-print(vykazy.head())
-print(zamestnanci.head())
+#print(vykazy.head())
+#print(zamestnanci.head())
 
 vykazy_zamestnanci = pandas.merge(vykazy, zamestnanci, how="cross")
-print(vykazy_zamestnanci.columns)
-print(vykazy_zamestnanci)
-
-#vykazy_zamestnanci_1= pandas.merge(vykazy, zamestnanci, how="left", on="emloyee_id"=="cislo_zamestnance")
-#print(vykazy_zamestnanci_1)
-
-#Následně proveď statistiku vykázaných hodin za jednotlivé kanceláře,
-# tj. spočítej celkový počet hodin vykázaný zaměstnanci jednotlivých kanceláří na projekty daného zákazníka.
-# dotaz: co je jednotlivá kancelář? město?
-# dotaz2: je totožný sloupec "employee_id" a "cislo_zamestnance"? ráda bych to použila pro merge, jen nevím...,
-# ...jak to udělat,když se jmenují odlišně -> jak je přejmenovat? nebo se to píše nějak do on="employee_id"="cislo_zamestnance"?
+#print(vykazy_zamestnanci.columns)
+#print(vykazy_zamestnanci)
+hodiny_projekt = vykazy_zamestnanci.groupby(["mesto", "project"])["hours"].sum()
+print(hodiny_projekt)
 
